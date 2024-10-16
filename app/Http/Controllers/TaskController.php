32bff217f53bb2de59d04ca1c70c19d71a017927
jawadali000123 <jawadali000123@gmail.com>
2024-10-16 07:39:05 +0000

    Add task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    private $tasks = [];
+
+    public function index(){
+        $tasks = session()->get('tasks', []);
+        return view('task/index', ['tasks' => $tasks]);
+    }
+
+    public function store(Request $request){
+        $tasks = session()->get('tasks', []);
+        $tasks[] = $request->input('task');
+        session()->put('tasks', $tasks);
+
+        return redirect('/');
+    }
+
+    public function edit($id){
+        $tasks = session()->get('tasks', []);
+        $task = $tasks[$id];
+
+        return view('task/edit', ['task' => $task, 'id' => $id]); 
+    }
+
+    public function update(Request $request, $id){
+        $tasks = session()->get('tasks', []);
+        $tasks[$id] = $request->input('task');
+        session()->put('tasks', $tasks);
+
+        return redirect('/');
+    }
+
+    public function delete($id){
+        $tasks = session()->get('tasks', []);
+        unset($tasks[$id]);
+        session()->put('tasks', $tasks);
+
+        return redirect('/');
+    }
+}
+
